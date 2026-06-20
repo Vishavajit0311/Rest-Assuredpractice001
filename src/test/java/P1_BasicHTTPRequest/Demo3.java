bce0763e1b4a7f730e606e3f200282318ed12c47
“Vishavajit0311” <Vishwa@Vishwa-MacBook-Air.local>
2026-06-20 15:54:55 +0530

    "demo3 file has been created"

---
+++ src/test/java/P1_BasicHTTPRequest/Demo3.java
@@ -0,0 +1,11 @@
+package P1_BasicHTTPRequest;
+
+import org.codehaus.groovy.transform.SourceURIASTTransformation;
+
+public class Demo3 {
+    static void main(String[] args) {
+
+        System.out.println("hiibhhbf ");
+
+    }
+}
